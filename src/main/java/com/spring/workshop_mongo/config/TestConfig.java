49259c3f4ea8257ea08dbaf0771deb0d6a2f3c9c
Guilherme <guilherme.1009@hotmail.com>
2025-05-19 17:28:31 -0300

    Models Author, Comment, Post

--- src/main/java/com/spring/workshop_mongo/config/TestConfig.java
+++ src/main/java/com/spring/workshop_mongo/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.spring.workshop_mongo.models.entities.User;
+import com.spring.workshop_mongo.respositories.PostRepository;
 import com.spring.workshop_mongo.respositories.UserRepository;
 
 import jakarta.annotation.PostConstruct;
@@ -17,10 +18,13 @@ public class TestConfig {
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
 	
 	@PostConstruct
 	public void init() {
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
---
+++ src/main/java/com/spring/workshop_mongo/respositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.spring.workshop_mongo.respositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.workshop_mongo.models.entities.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
